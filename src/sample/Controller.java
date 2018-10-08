31d09651b005ab3ddc7a5dc9c6ecacb1f24a3609
Martyna Kania <martynaakaniaa@gmail.com>
2018-10-09 00:49:52 +0200

    line is drawn on second click

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -2,31 +2,27 @@ package sample;
 
 import javafx.application.Application;
 import javafx.event.EventHandler;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Line;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import sample.model.Point;
 
 import java.io.IOException;
 import java.util.List;
 
-import static javafx.scene.paint.Color.GRAY;
-import static javafx.scene.paint.Color.GREEN;
-import static javafx.scene.paint.Color.RED;
-
 public class Controller extends Application {
     private Pane pane = new Pane();
     private ScrollPane scrollPane = new ScrollPane();
     private static List<Point> pointList = null;
-    private static int startIndex = 0;
 
-    private final int factor = 5;
+    private String chosenFigure = "Line";
+    private Point firstClick = null;
+    private Point secondClick = null;
 
     public static void main(String[] args) {
         launch(args);
@@ -36,8 +32,15 @@ public class Controller extends Application {
     public void start(Stage primaryStage) throws IOException {
 
         pane.setOnMouseClicked((EventHandler<MouseEvent>) event -> {
-            Circle circle = new Circle(event.getX(), event.getY(), 2);
-            pane.getChildren().add(circle);
+            if (firstClick == null) {
+                firstClick = new Point(event.getX(), event.getY());
+                pane.getChildren().add(new Circle(event.getX(), event.getY(), 0.3));
+            } else {
+                secondClick = new Point(event.getX(), event.getY());
+                Node node = draw();
+                pane.getChildren().add(node);
+                cleanClicks();
+            }
         });
         scrollPane.setContent(pane);
 
@@ -48,18 +51,20 @@ public class Controller extends Application {
 
     }
 
-    private void drawLines() {
-        int index = startIndex;
-        for (int i = 0; i < pointList.size() - 1; ++i) {
-            Point start = pointList.get(index);
-            ++index;
-            if (index == pointList.size()) {
-                index = 0;
-            }
-            Point end = pointList.get(index);
-            pane.getChildren().add(new Line(start.getX() * factor, start.getY() * factor, end.getX() * factor, end.getY() * factor));
+    private Node draw() {
+        switch (chosenFigure) {
+            case "Line":
+                return drawLine();
         }
+        return null;
     }
 
+    private Node drawLine() {
+        return new Line(firstClick.getX(), firstClick.getY(), secondClick.getX(), secondClick.getY());
+    }
 
+    private void cleanClicks() {
+        secondClick = null;
+        firstClick = null;
+    }
 }
