88be5b1671567a960d056faa23656dbf17315a4f
Martyna Kania <martynaakaniaa@gmail.com>
2018-10-09 02:13:50 +0200

    allow user to choose figure

--- src/sample/MainController.java
+++ src/sample/MainController.java
@@ -1,8 +1,11 @@
 package sample;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+
 import javafx.scene.layout.Pane;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Line;
@@ -10,15 +13,23 @@ import javafx.scene.shape.Rectangle;
 import sample.model.Point;
 
 
+
 public class MainController {
     @FXML
     private Pane mainPane;
+    @FXML
+    private TextField thicknessText;
+    @FXML
+    private TextField colorText;
+    @FXML
+    private TextField chosenFigureMsg;
 
     private enum Figure {LINE, CIRCLE, RECTANGLE}
 
     private Figure chosenFigure = Figure.CIRCLE;
     private Point firstClick = null;
     private Point secondClick = null;
+    private final String chosenFigureMsgTemplate = "Chosen figure: ";
 
     public void chosePoint(MouseEvent event) {
         if (firstClick == null) {
@@ -57,6 +68,24 @@ public class MainController {
 
     private Node drawLine() {
         return new Line(firstClick.getX(), firstClick.getY(), secondClick.getX(), secondClick.getY());
+    }
+
+    public void switchToRectangle(ActionEvent actionEvent) {
+        chosenFigure=Figure.RECTANGLE;
+        chosenFigureMsg.setText(chosenFigureMsgTemplate + "Rectangle");
+    }
+
+    public void switchToCircle(ActionEvent actionEvent) {
+        chosenFigure=Figure.CIRCLE;
+        chosenFigureMsg.setText(chosenFigureMsgTemplate + "Circle");
+    }
+
+    public void switchToLine(ActionEvent actionEvent) {
+        chosenFigure=Figure.LINE;
+        chosenFigureMsg.setText(chosenFigureMsgTemplate + "Line");
+    }
+
+    public void editFigure(ActionEvent actionEvent) {
     }
 
 
